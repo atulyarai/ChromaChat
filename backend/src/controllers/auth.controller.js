89cb25a9e6d96c064d4e669d77a41a8d8fd222ec
Atulya Rai <atulya.kush@gmail.com>
2025-01-27 12:36:20 +0530

    Remove .gitignore, add package-lock.json, configure Tailwind CSS, implement message routes, and enhance authentication with profile update and checkAuth functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -60,7 +61,7 @@ export const login = async (req, res) => {
     generateToken(user._id, res);
     res.status(200).json({
       _id: user._id,
-      fullName: user.email,
+      fullName: user.fullName,
       email: user.email,
       profilePic: user.profilePic,
     });
@@ -80,4 +81,34 @@ export const logout = (req, res) => {
   }
 };
 
-export const updateProfile = async (req, res) => {};
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic)
+      return res.status(400).json({ message: "Profile Pic is required" });
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    console.log("error in update profile:", err.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const checkAuth = async (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (err) {
+    console.log("Error in checkAuth controller", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
@@ -10,9 +11,10 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use((req, res) => {
   res.send("Listening");
